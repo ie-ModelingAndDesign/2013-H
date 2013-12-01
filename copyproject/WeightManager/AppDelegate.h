a6ecf6bf391aade6b3bbfb8d8e2578fbdf6fc529
e125740 <IhaYukiya@iha-no-MacBook-Pro-2.local>
2013-12-01 18:08:10 +0900

    Add copyproject

---
+++ copyproject/WeightManager/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  WeightManager
+//
+//  Created by e125740 on 2013/12/01.
+//  Copyright (c) 2013年 Yukiya IHA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
