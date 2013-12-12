020805c56bfbf5122589051ff65dbd0327dcf497
Yume OOHAMA <e125729@Yume-no-MacBook-Pro.local>
2013-12-12 11:49:12 +0900

    add class figure

---
+++ Object/Group_UI/WeightManager/WeightManager/WeightInput.h
@@ -0,0 +1,23 @@
+//
+//　今日の体重
+//  WeightInput.h
+//  WeightManager
+//
+//  Created by Chie AHAREN on 2013/12/04.
+//  Copyright (c) 2013年 Yukiya IHA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WeightInput : UIViewController{
+    IBOutlet UILabel *label;
+    int count;
+    IBOutlet UILabel *label2;
+	BOOL startInput;
+    BOOL Input;
+}
+- (IBAction)button:(id)sender;
+- (IBAction)clearButton:(id)sender;
+- (IBAction)enter:(id)sender;
+
+@end
