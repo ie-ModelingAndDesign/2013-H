b3d864e806099e9ffbd7d3e5310599b19a004d2e
aharenchie <e125731@ie.u-ryukyu.ac.jp>
2014-01-16 12:16:33 +0900

    rm

--- Weekly/1_9/UI/WeightManager/WeightManager/BarAndScatterViewController.h
+++ Weekly/1_9/UI/WeightManager/WeightManager/BarAndScatterViewController.h
@@ -7,7 +7,21 @@
 //
 
 #import <UIKit/UIKit.h>
+#import "CorePlot-CocoaTouch.h"
 
+// 折れ線グラフ用のデータソース(CPTPlotDataSource)を記載。CPTScatterPlotDataSourceと間違えないように注意
 @interface BarAndScatterViewController : UIViewController
+<CPTPlotDataSource>
+{
+@private
+    // グラフ表示領域（この領域に円グラフを追加する）
+    CPTGraph *graph;
+
+    __weak IBOutlet UILabel *label;
+}
+
+// 円グラフで表示するデータを保持する配列
+@property (readwrite, nonatomic) NSMutableArray *scatterPlotData;
+@property (readwrite, nonatomic) NSMutableArray *Mokuhyo;
 
 @end
