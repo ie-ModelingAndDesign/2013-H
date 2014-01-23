03a59729d005c42477212b72428e0cb07f6c1105
CR7 <e125753@CR7.local>
2014-01-23 14:03:02 +0900

    WeightManager

---
+++ Weekly/1_23/WeightManager/WeightManager/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  WeightManager
+//
+//  Created by CR7 on 2014/01/23.
+//  Copyright (c) 2014年 syuhei miyagi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
